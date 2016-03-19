5ee18122511b9a78a22f7db387f00c5110327f80
cchisnel <cchisnel@umflint.edu>
2016-03-19 17:30:17 -0400

    list view added(nonfunctional)

--- app/src/main/java/com/capstone/hammond/wallstreetfantasyleaguefinal/CreateJoinLeagueFragment.java
+++ app/src/main/java/com/capstone/hammond/wallstreetfantasyleaguefinal/CreateJoinLeagueFragment.java
@@ -12,11 +12,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -38,6 +42,8 @@ public class CreateJoinLeagueFragment extends Fragment {
 
     EditText mLeagueName;
     Button mSignUpButton;
+    String userSelection, LeagueName;
+    private static final Logger logger = Logger.getLogger(BuyFragment.class.getName());
 
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -46,6 +52,51 @@ public class CreateJoinLeagueFragment extends Fragment {
         mSignUpButton = (Button) view.findViewById(R.id.btnjoinleague);
         mLeagueName = (EditText) view.findViewById(R.id.txtTeamName);
 
+
+        final ListView leaguesL = (ListView) view.findViewById(R.id.leagueList);
+
+
+
+
+
+        updateLeagueList(leaguesL);
+
+        leaguesL.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                //grabs league name from user selection
+                userSelection = ((TextView) v).getText().toString();
+                LeagueName = userSelection.substring(userSelection.indexOf("League") + 5, userSelection.indexOf(","));
+            }
+        });
+    }
+
+    private void updateLeagueList(ListView leaguesL) {
+        // Creates and populates an ArrayList of objects from parse
+        final ArrayAdapter<String> listAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1);
+        leaguesL.setAdapter(listAdapter);
+
+        //Queries parse for the users League information
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("_User");
+        query.whereEqualTo("UserID", ParseUser.getCurrentUser().getObjectId());
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> leaguesL, com.parse.ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < leaguesL.size(); i++) {
+
+                        String parseLeague = leaguesL.get(i).get("League").toString();
+
+
+                        listAdapter.add(" " + parseLeague);
+                    }
+
+                } else {
+                    Toast.makeText(getActivity(), "Error", Toast.LENGTH_LONG).show();
+                     logger.log(Level.SEVERE, e.toString());
+                }
+            }
+
+        });
 
 
 
@@ -71,7 +122,6 @@ public class CreateJoinLeagueFragment extends Fragment {
                         }
                     }
                 });
-
 
 
             }
